d07ed61158462a0df8a30727bb0e08d2f4929e84
danielmoralesp <daniel_moralesp@hotmail.com>
2019-07-09 20:42:13 -0500

    Ticket #2 - New and Create Methods

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,4 +10,19 @@ class ProductsController < ApplicationController
   def new
   	@product = Product.new
   end
+
+  def create
+  	@product = Product.new(product_params)
+
+  	if @product.save
+  		redirect_to product_path(@product)
+  	else
+  		render :new
+  	end
+  end
+
+  private
+  	def product_params
+  		params.require(:product).permit(:name, :description, :price, :image_product, :user_id, :color)
+  	end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,13 +1,12 @@
 class Product < ApplicationRecord
 	validates :name, :price, :description, presence: true
-	validates :price, numericality: { 
-											only_float: true
-											# greater_than: 50,
-											# greater_than_or_equal_to: 51
-										}
-	validates :price, length: { in: 4..7 }
-	validates :name, length: { minimum: 50 }
-	validates :description, format: { with: /\A[a-zA-Z]/, message: "only allows letters"}
+	# validates :price, numericality: { 
+	# 										only_float: true
+	# 										# greater_than: 50,
+	# 										# greater_than_or_equal_to: 51
+	# 									}
+	# validates :price, length: { in: 4..7 }
+	# validates :name, length: { minimum: 50 }
 
 	# has_many :comments
 	belongs_to :user
